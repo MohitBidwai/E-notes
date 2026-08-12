4fddbc38d21ab9d205ec4eb1a88c15c6eda71aa8
Mohit Bidwai <mohit.19bidwai@gmail.com>
2026-08-12 21:15:32 +0530

    Added swagger dependency to the project

--- src/main/java/com/mohit/e_notes/projectConfig/ProjectConfig.java
+++ src/main/java/com/mohit/e_notes/projectConfig/ProjectConfig.java
@@ -4,6 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
 public class ProjectConfig {
 
@@ -13,4 +17,15 @@ public class ProjectConfig {
 		return new ModelMapper();
 	}
 	
+	@Bean
+	public OpenAPI openApiConfig() {
+	    return new OpenAPI()
+	            .info(new Info()
+	                    .title("E-Notes API")
+	                    .description("REST API documentation for E-Notes application")
+	                    .version("1.0.0")
+	                    .contact(new Contact()
+	                            .name("Mohit Bidwai")
+	                            .email("mohit.19bidwai@gmail.com")));
+	}
 }
